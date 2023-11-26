25c2753ae681bdbd5af4ffc64aa221952d7aa0c0
Jacob Friis Saxberg <jacob@webcom.dk>
2023-11-26 18:28:18 +0100

    use local key

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 import { test } from 'bun:test';
-import { generateKeyPairSync } from 'node:crypto';
+// import { generateKeyPairSync } from 'node:crypto';
 // import { createPrivateKey } from 'node:crypto';
 import { readFile } from 'node:fs/promises';
 import { writeFile } from 'node:fs/promises';
@@ -11,9 +11,9 @@ import makeFirmware from '../src/main';
 const firmwarePath =
   '../spaceinvader/openhaystack/OpenHaystack/OpenHaystack/HaystackApp/Firmwares/Moko/nrf52810_xxaa.bin';
 const pattern = 'OFFLINEFINDINGPUBLICKEYHERE!';
-// const privateKeyPath = '../private.key';
+const privateKeyPath = '../private.key';
 // const privateKeyPath = '/Users/jacob/Downloads/test.pem';
-const { privateKey } = generateKeyPairSync('ec', { namedCurve: 'prime256v1' });
+// const { privateKey } = generateKeyPairSync('ec', { namedCurve: 'prime256v1' });
 
 // function keyBufferToKeyObject(key: Buffer) {
 //   return createPrivateKey({ key });
@@ -23,6 +23,7 @@ test('makeFirmware', async () => {
   const accessory = makeAccessory();
   const firmwareBuffer = await readFile(firmwarePath);
   // const privateKey = keyBufferToKeyObject(await readFile(privateKeyPath));
+  const privateKey = await readFile(privateKeyPath);
   const zipBuffer = await makeFirmware(
     accessory,
     firmwareBuffer,
