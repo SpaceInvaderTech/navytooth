61ffaa4879c5756589e1fefce7d8f52c5915f5dc
Jacob Friis Saxberg <jacob@webcom.dk>
2023-12-02 20:49:49 +0100

    WIP

--- test/crypto.test.ts
+++ test/crypto.test.ts
@@ -1,9 +1,9 @@
 import { test, expect } from 'bun:test';
 import { readFile } from 'node:fs/promises';
-import { createECDH, createPrivateKey, createVerify } from 'node:crypto';
+import { createECDH, createVerify } from 'node:crypto';
 import { getAdvertisementKey, signData } from '../src/crypt';
 
-const privateKeyPath = '../private.key';
+const privateKeyPath = '../private.pem';
 
 // EA140CF93E0D
 // const privateKeyBase64Hex = 'bwaa+RfcHRvgbfwjBRKBuHVAW2wEHlrlPNpvDw==';
@@ -13,6 +13,17 @@ const privateKeyBigInt = BigInt(privateKeyDecimal);
 const privateKeyBuffer = Buffer.from(privateKeyBigInt.toString(16), 'hex');
 const publicKeyHex = 'b65b3f96cee7bd97e7df750804bcbabf2bbbe9ced32b9fad45bea396';
 
+// export function makePEM(key: Buffer) {
+//   const regexPattern = new RegExp('.{1,64}', 'g');
+//   const keyLines = key.toString('base64').match(regexPattern);
+//   if (keyLines === null) throw new Error('Invalid key');
+//   return [
+//     '-----BEGIN EC PRIVATE KEY-----',
+//     ...keyLines,
+//     '-----END EC PRIVATE KEY-----',
+//   ].join('\n');
+// }
+
 // https://github.com/NordicSemiconductor/pc-nrfutil/blob/master/nordicsemi/dfu/signing.py#L103-L119
 function verify(initPacket: Buffer, signature: Buffer, privateKey: string) {
   // Create verifier with SHA256
@@ -21,23 +32,6 @@ function verify(initPacket: Buffer, signature: Buffer, privateKey: string) {
   // Verify signature
   return verifier.verify(privateKey, signature);
 }
-
-test.todo('key buffer = KeyObject', async () => {
-  const exchange = createECDH('secp224r1');
-  exchange.generateKeys();
-  const privateKey = exchange.getPrivateKey();
-  const privateKeyObject = createPrivateKey(privateKey);
-  // const privateKeyObject = createPrivateKey({
-  //   key: privateKey.toString(),
-  //   type: 'pkcs8',
-  //   format: 'pem',
-  // });
-  const privateKeyExport = privateKeyObject.export({
-    type: 'pkcs8',
-    format: 'pem',
-  });
-  expect(privateKey).toStrictEqual(privateKeyExport);
-});
 
 test('key remake', async () => {
   const exchange = createECDH('secp224r1');
@@ -69,9 +63,8 @@ test('key works', async () => {
   expect(publicKey.length).toBe(29);
 });
 
-test.todo('sign & verify', async () => {
+test('sign & verify', async () => {
   const privateKeyBuffer = await readFile(privateKeyPath);
-  // const privateKey = createPrivateKey(privateKeyBuffer);
   const data = Buffer.from('The Love Boat');
   const signature = signData(data, privateKeyBuffer);
   expect(signature.length).toBeTruthy();
