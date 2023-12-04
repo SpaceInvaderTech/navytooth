2367822970a60095ac73996cdc7d21425e259710
Jacob Friis Saxberg <jacob@webcom.dk>
2023-12-04 13:48:00 +0100

    sanity test

---
+++ test/sanity.test.ts
@@ -0,0 +1,34 @@
+import { describe, test, expect } from 'bun:test';
+import { readFile } from 'node:fs/promises';
+import { getAdvertisementKey, hashFirmware } from '../src/crypt';
+
+const firmwarePath = './test/EA140CF93E0D/firmware.bin';
+// const privateKeyPath = '../private.pem';
+
+describe('Node vs Python', () => {
+  test('keys', () => {
+    // from plist for EA140CF93E0D
+    const privateKeyBuffer = Buffer.from(
+      'bwaa+RfcHRvgbfwjBRKBuHVAW2wEHlrlPNpvDw==',
+      'base64',
+    );
+    // from test.py
+    const advertisementKeyMatch = Buffer.from(
+      '02b65b3f96cee7bd97e7df750804bcbabf2bbbe9ced32b9fad45bea396',
+      'hex',
+    ).subarray(1);
+    const advertisementKey = getAdvertisementKey(privateKeyBuffer);
+    expect(advertisementKey.byteLength).toBe(28);
+    expect(advertisementKeyMatch.byteLength).toBe(28);
+    expect(advertisementKey).toStrictEqual(advertisementKeyMatch);
+  });
+
+  test.skip('hash', async () => {
+    const firmware = await readFile(firmwarePath);
+    const firmwareHash = hashFirmware(firmware).toString('hex');
+    // from test.py or nrfutil pkg display
+    const hashMatch =
+      '4d67ba795ad5e543f1495443c966eb624a7531a890903df4da0c779ccf5afd3c';
+    expect(firmwareHash).toBe(hashMatch);
+  });
+});
--- test/test.py
+++ test/test.py
@@ -2,8 +2,12 @@ from codecs import encode
 from base64 import b64decode
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.backends import default_backend
+import hashlib
 
 # python3 test/test.py
+
+# Base64 encoded private key data
+privateKey64 = "bwaa+RfcHRvgbfwjBRKBuHVAW2wEHlrlPNpvDw==" 
 
 def bytes_to_int(b):
     return int(encode(b, "hex"), 16)
@@ -11,30 +15,40 @@ def bytes_to_int(b):
 def get_compressed_public_key(priv):
     # Derive the private key
     private_key = ec.derive_private_key(priv, ec.SECP224R1(), default_backend())
-    
     # Get the full public key
     public_key = private_key.public_key()
     public_numbers = public_key.public_numbers()
-    
     # Extract x and y coordinates
     x = public_numbers.x
     y = public_numbers.y
-
     # Determine if y is even or odd
     prefix = '02' if y % 2 == 0 else '03'
-
     # Compress and format the public key
     compressed_public_key = f'{prefix}{x:056x}'
     return compressed_public_key
 
-# Base64 encoded private key data
-data = "bwaa+RfcHRvgbfwjBRKBuHVAW2wEHlrlPNpvDw=="
+def test_keys():
+    # Convert to integer
+    private_key = bytes_to_int(b64decode(privateKey64))
+    # Get compressed public key
+    compressed_public_key = get_compressed_public_key(private_key)
+    print('Private Key:', private_key)
+    print('Compressed Public Key:', compressed_public_key)
 
-# Convert to integer
-private_key = bytes_to_int(b64decode(data))
+def test_hash():
+    firmware_filename = "./firmware/firmware.bin"
+    read_buffer = 4096
+    digest = hashlib.sha256()
+    with open(firmware_filename, 'rb') as firmware_file:
+        while True:
+            data = firmware_file.read(read_buffer)
+            if data:
+                digest.update(data)
+            else:
+                break
+    sha = digest.digest()
+    hashLittleEndian = sha[31::-1]
+    print('Hash:', hashLittleEndian.hex())
 
-# Get compressed public key
-compressed_public_key = get_compressed_public_key(private_key)
-
-print('Private Key:', private_key)
-print('Compressed Public Key:', compressed_public_key)
+test_keys()
+test_hash()
