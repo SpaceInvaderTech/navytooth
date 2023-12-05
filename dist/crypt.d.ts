29402873cff518d5694feb6b32e5b76d32edfba9
Jacob Friis Saxberg <jacob@webcom.dk>
2023-12-05 11:35:17 +0100

    build

--- dist/crypt.d.ts
+++ dist/crypt.d.ts
@@ -3,5 +3,5 @@ import { Buffer } from 'node:buffer';
 export declare function makePrivateKey(): Buffer;
 export declare function getAdvertisementKey(privateKey: Buffer): Buffer;
 export declare function hashFirmware(firmwarePatched: Buffer): Buffer;
-export declare function endianSwap(data: Uint8Array): Buffer;
+export declare function endianSwap(signature: Uint8Array): Buffer;
 export declare function signData(data: Uint8Array, privateKey: Buffer): Buffer;
